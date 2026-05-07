b64337f962c302783b2e577ccb30feb439b1ba00
lan0601 <allantinambacanjr@gmail.com>
2026-05-08 00:22:14 +0800

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
                                 id="docs-card"
                                 class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
-                                <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
+                                <!-- <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
                                     <img
                                         src="https://laravel.com/assets/img/welcome/docs-light.svg"
                                         alt="Laravel documentation screenshot"
@@ -85,7 +85,7 @@
                                     <div
                                         class="absolute -bottom-16 -left-16 h-40 w-[calc(100%+8rem)] bg-gradient-to-b from-transparent via-white to-white dark:via-zinc-900 dark:to-zinc-900"
                                     ></div>
-                                </div>
+                                </div> -->
 
                                 <div class="relative flex items-center gap-6 lg:items-end">
                                     <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
